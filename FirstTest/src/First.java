07ceb8b7a1345d9719a60d616e1b1548757d0175
malimin <1052163211@qq.com>
2021-01-23 20:58:45 +0800

    Initial commit

---
+++ FirstTest/src/First.java
@@ -0,0 +1,11 @@
+/**
+ * @author Ma Limin
+ * @version 1.0
+ * @date 2021-1-23 - 20:47
+ * @description PACKAGE_NAME
+ */
+public class First {
+    public static void main(String[] args) {
+        System.out.println("Hello Java!");
+    }
+}
